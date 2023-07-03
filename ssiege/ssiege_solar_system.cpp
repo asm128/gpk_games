13e2856c4ed78277ef46a1f5c1df3227dc9727d5
asm128 <pabloarielz@outlook.com>
2023-07-03 17:23:50 -0300

    * Fixed json booleans.

--- ssiege/ssiege_solar_system.cpp
+++ ssiege/ssiege_solar_system.cpp
@@ -98,52 +98,48 @@ static	::gpk::error_t	printPlanet		(const ::ssiege::SDetailPlanet & body) {
 }
 
 static	::gpk::error_t	loadPlanet		(::ssiege::SDetailPlanet & bodyData, const ::gpk::SJSONReader & jsonData, int32_t jsonIndexStellarBody) { 
-	double						dealiased;
-	int32_t						indexProperty;
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mass"					)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Mass					= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "diameter"				)) else { bodyData.Diameter				= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "density"				)) else { bodyData.Density				= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "gravity"				)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Gravity				= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "escape_velocity"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.EscapeVelocity		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "rotation_period"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.RotationPeriod		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "length_of_day"			)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.LengthOfDay			= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "distance_from_sun"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.DistanceFromSun		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "perihelion"				)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Perihelion			= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "aphelion"				)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Aphelion				= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "orbital_period"			)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.OrbitalPeriod		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "orbital_velocity"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.OrbitalVelocity		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "orbital_inclination"	)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.OrbitalInclination	= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "orbital_eccentricity"	)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.OrbitalEccentricity	= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "obliquity_to_orbit"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.ObliquityToOrbit		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mean_temperature"		)) else { bodyData.MeanTemperature		= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "surface_pressure"		)) else { bodyData.SurfacePressure		= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "number_of_moons"		)) else { bodyData.NumberOfMoons		= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "ring_system"			)) else { bodyData.RingSystem			= ::gpk::char2bool(jsonData.View[indexProperty]); }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "global_magnetic_field"	)) else { bodyData.GlobalMagneticField	= ::gpk::char2bool(jsonData.View[indexProperty]); }
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "mass"					, bodyData.Mass					));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "diameter"				, bodyData.Diameter				));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "density"				, bodyData.Density				));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "gravity"				, bodyData.Gravity				));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "escape_velocity"		, bodyData.EscapeVelocity		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "rotation_period"		, bodyData.RotationPeriod		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "length_of_day"		, bodyData.LengthOfDay			));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "distance_from_sun"	, bodyData.DistanceFromSun		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "perihelion"			, bodyData.Perihelion			));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "aphelion"				, bodyData.Aphelion				));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "orbital_period"		, bodyData.OrbitalPeriod		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "orbital_velocity"		, bodyData.OrbitalVelocity		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "orbital_inclination"	, bodyData.OrbitalInclination	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "orbital_eccentricity"	, bodyData.OrbitalEccentricity	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "obliquity_to_orbit"	, bodyData.ObliquityToOrbit		));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "mean_temperature"		, bodyData.MeanTemperature		));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "surface_pressure"		, bodyData.SurfacePressure		));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "number_of_moons"		, bodyData.NumberOfMoons		));
+	ws_if_failed(::gpk::jsonObjectGetBoolean(jsonData, jsonIndexStellarBody, "ring_system"			, bodyData.RingSystem			));
+	ws_if_failed(::gpk::jsonObjectGetBoolean(jsonData, jsonIndexStellarBody, "global_magnetic_field", bodyData.GlobalMagneticField	));
 	return 0; 
 }
 
 static	::gpk::error_t	loadStar		(::ssiege::SDetailStar & bodyData, const ::gpk::SJSONReader & jsonData, int32_t jsonIndexStellarBody) { 
-	double						dealiased;
-	int32_t						indexProperty;
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mass"					)) else { bodyData.Mass					= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "gm"						)) else { bodyData.Gm					= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "volume"					)) else { bodyData.Volume				= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "volumetric_mean_radius"	)) else { bodyData.VolumetricMeanRadius	= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mean_density"			)) else { bodyData.MeanDensity			= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "surface_gravity"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.SurfaceGravity		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "escape_velocity"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.EscapeVelocity		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "ellipticity"			)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Ellipticity			= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "moment_of_inertia"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.MomentOfInertia		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "visual_magnitude"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.VisualMagnitude		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "absolute_magnitude"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.AbsoluteMagnitude	= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "luminosity"				)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.Luminosity			= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mass_conversion_rate"	)) else { bodyData.MassConversionRate	= (uint32_t)jsonData[indexProperty]->Token->Value; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "mean_energy_production"	)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.MeanEnergyProduction	= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "surface_emission"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.SurfaceEmission		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "central_pressure"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.CentralPressure		= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "central_temperature"	)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.CentralTemperature	= (float)dealiased; }
-	ws_if_failed(indexProperty = ::gpk::jsonObjectValueGet(jsonData, jsonIndexStellarBody, "central_density"		)) else { memcpy(&dealiased, &jsonData[indexProperty]->Token->Value, sizeof(double)); bodyData.CentralDensity		= (float)dealiased; }
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "mass"						, bodyData.Mass					));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "gm"						, bodyData.Gm					));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "volume"					, bodyData.Volume				));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "volumetric_mean_radius"	, bodyData.VolumetricMeanRadius	));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "mean_density"				, bodyData.MeanDensity			));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "surface_gravity"			, bodyData.SurfaceGravity		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "escape_velocity"			, bodyData.EscapeVelocity		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "ellipticity"				, bodyData.Ellipticity			));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "moment_of_inertia"		, bodyData.MomentOfInertia		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "visual_magnitude"			, bodyData.VisualMagnitude		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "absolute_magnitude"		, bodyData.AbsoluteMagnitude	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "luminosity"				, bodyData.Luminosity			));
+	ws_if_failed(::gpk::jsonObjectGetInteger(jsonData, jsonIndexStellarBody, "mass_conversion_rate"		, bodyData.MassConversionRate	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "mean_energy_production"	, bodyData.MeanEnergyProduction	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "surface_emission"			, bodyData.SurfaceEmission		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "central_pressure"			, bodyData.CentralPressure		));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "central_temperature"		, bodyData.CentralTemperature	));
+	ws_if_failed(::gpk::jsonObjectGetDecimal(jsonData, jsonIndexStellarBody, "central_density"			, bodyData.CentralDensity		));
 	return 0; 
 }
 
@@ -183,6 +179,7 @@ static	::gpk::error_t	loadStar		(::ssiege::SDetailStar & bodyData, const ::gpk::
 			gpk_necs(solarSystem.Body.push_back(stellarBodyNames[iPlanet], bodyProperties));
 		}
 	}
+
 	for(uint32_t iPlanet = 0; iPlanet < solarSystem.Body.Keys.size(); ++iPlanet)
 		gpk_necs(solarSystem.Parent.push_back(solarSystem.Body.Keys.find(bodyParentNames[iPlanet])));
 
@@ -205,7 +202,7 @@ static	::gpk::error_t	loadStar		(::ssiege::SDetailStar & bodyData, const ::gpk::
 }
 
 ::gpk::error_t			ssiege::solarSystemSetup		(::ssiege::SSolarSystem & solarSystem, ::gpk::SEngine & engine, ::gpk::vcc jsonFilePath) {
-	::gpk::SJSONFile			jsonFile;
+	::gpk::SJSONFile			jsonFile						= {};
 	gpk_necs(::gpk::jsonFileRead(jsonFile, jsonFilePath));
 	return ::ssiege::solarSystemSetup(solarSystem, engine, jsonFile.Reader);
 }
